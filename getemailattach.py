6e435f552217d3568867ddc48347e93b4f1c7f59
StoneArk <stoneark86@gmail.com>
2017-06-05 16:54:25 +0800

    Add "-d" parameter to delete source files after completion.

--- getemailattach.py
+++ getemailattach.py
@@ -3,14 +3,20 @@ import email
 import os
 
 if len(sys.argv) < 2:
-	print 'getemailattach.py <SOURCE_MIME_PATH> [<DESTINATION_FOLDER_PATH>]'
+	print 'getemailattach.py <SOURCE_MIME_PATH> [<DESTINATION_FOLDER_PATH>] [-d]'
 	quit()
 
 arrSrcFile = []
 destFolder = ''
+ifDelete = False
 srcMIME = sys.argv[1]
-if (len(sys.argv) > 2):
-	destFolder = sys.argv[2]
+if len(sys.argv) > 2:
+	if sys.argv[2] == '-d':
+		ifDelete = True
+	else:
+		destFolder = sys.argv[2]
+		if len(sys.argv) > 3 and sys.argv[3] == '-d':
+			ifDelete = True
 
 if os.path.isfile(srcMIME):
 	arrSrcFile = [srcMIME]
@@ -24,9 +30,11 @@ for mimeFileItem in arrSrcFile:
 		continue
 	attachmentCount = len(mimeMsg.get_payload())
 	if attachmentCount < 2:
-		print 'No attachment found in this email'
-		quit()
+		print('No attachment found in this email: ' + mimeFileItem)
+		continue
 	for i in range(1, attachmentCount):
 		attachmentItem = mimeMsg.get_payload()[i]
 		attachmentFilename = attachmentItem.get_filename()
 		open(os.path.join(destFolder, attachmentFilename), 'wb').write(attachmentItem.get_payload(decode=True))
+	if ifDelete == True:
+		os.remove(mimeFileItem)
